215b0248377645f38ea615e94ff1c2555fb58598
ChieChie <104299226+ChieChieBun@users.noreply.github.com>
2025-05-19 01:40:52 -0400

    Get routes

--- db/migrate/20250409012416_create_journals.rb
+++ db/migrate/20250409012416_create_journals.rb
@@ -2,8 +2,10 @@ class CreateJournals < ActiveRecord::Migration[8.0]
   def change
     create_table :journals do |t|
       t.string :title
+      t.string :entry
       t.boolean :private
       t.references :categories, null:false, foreign_key: true
+      t.references :user, null:false, foreign_key: true
       t.timestamps
     end
   end
